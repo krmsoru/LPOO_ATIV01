65e42690e0400effd718076a22d7d2fdddef7bff
aka.dnji <utaykkon@hotmail.com>
2021-02-23 22:11:03 -0300

    POO_ativ01 v0

---
+++ src/soma/Soma.java
@@ -0,0 +1,11 @@
+package soma;
+
+public class Soma
+{
+    public static void main(String[] args)
+    {
+        frmPrincipal frmP = new frmPrincipal(null, true);
+        frmP.setVisible(true);
+    }
+    
+}
